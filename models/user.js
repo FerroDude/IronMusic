ad005506617ca81497198a30cfc8a1e3f8b12498
Juno Jo <juno@juno.pro>
2021-09-14 14:52:58 -0400

    account creation complete

--- models/user.js
+++ models/user.js
@@ -2,34 +2,40 @@
 
 const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    trim: true
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true
+    },
+    email: {
+      type: String,
+      required: true,
+      lowercase: true,
+      trim: true
+    },
+    passwordHashAndSalt: {
+      type: String
+    },
+    isArtist: {
+      type: String,
+      required: true
+    },
+    links: {
+      type: Array
+    },
+    picture: {
+      type: String
+    },
+    photoGallery: {
+      type: Array
+    },
+    description: {
+      type: String
+    }
   },
-  email: {
-    type: String,
-    required: true,
-    lowercase: true,
-    trim: true
-  },
-  passwordHashAndSalt: {
-    type: String
-  },
-  isArtist: {
-    type: Boolean,
-    required: true
-  },
-  links: {
-    type: Array
-  },
-  picture: {
-    type: String
-  },
-  photoGallery: {
-    type: Array
-  }
-});
+  { timestamps: true }
+);
 
 const User = mongoose.model('User', userSchema);
 
