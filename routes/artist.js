389464577aca49e51655fc82afa362b45b872188
Pedro Ferreira <pedroferrodude@hotmail.com>
2021-09-18 09:23:50 +0100

    added some search features to platform created artists

--- routes/artist.js
+++ routes/artist.js
@@ -1,11 +1,31 @@
 'use strict';
+const User = require('../models/user');
 
 const Router = require('express');
 
 const artistRouter = new Router();
 
-artistRouter.get('/search', (req, res) => {
-  res.render('artist/search');
+artistRouter.get('/search', (req, res, next) => {
+  User.find({ isArtist: true })
+    .then((artists) => {
+      res.render('artist/search', { artists });
+    })
+    .catch((error) => {
+      next(error);
+    });
+});
+
+artistRouter.get('/search-results', (req, res, next) => {
+  const { artist } = req.query;
+  console.log(artist);
+  User.findOne({ name: artist })
+    .then((result) => {
+      console.log(result);
+      res.render('artist/search-results', { result });
+    })
+    .catch((error) => {
+      next(error);
+    });
 });
 
 module.exports = artistRouter;
