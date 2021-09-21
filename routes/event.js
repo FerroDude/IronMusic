9d57518cef4a31c0a6038f2b399b696de96ccc86
Juno Jo <juno@juno.pro>
2021-09-21 15:19:15 -0400

    add event done

--- routes/event.js
+++ routes/event.js
@@ -15,6 +15,42 @@ eventRouter.get('/add', routeGuard, (req, res, next) => {
   res.render('event/create');
 });
 
+eventRouter.post(
+  '/add',
+  routeGuard,
+  upload.single('images'),
+  (req, res, next) => {
+    const location = [
+      {
+        city: req.body.city,
+        state: req.body.state,
+        country: req.body.country,
+        coordinates: [req.body.longitude, req.body.latitude]
+      }
+    ];
+    const { title, venue, date, description } = req.body;
+    let images;
+    if (req.file) {
+      images = [req.file.path];
+    }
+    Event.create({
+      title,
+      location,
+      venue,
+      date,
+      description,
+      images,
+      creator: req.user._id
+    })
+      .then(() => {
+        res.redirect('/');
+      })
+      .catch((error) => {
+        next(error);
+      });
+  }
+);
+
 eventRouter.get('/edit', routeGuard, (req, res, next) => {
   res.render('event/edit');
 });
