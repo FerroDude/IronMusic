62c605953536edac9383b64f19fcfc0b0bb8556f
jamiemn86 <jamie.a.mollernielsen@gmail.com>
2021-09-21 20:53:50 +0200

    Update to models/audio.js

--- models/audio.js
+++ models/audio.js
@@ -1,1 +1,19 @@
+const mongoose = require('mongoose');
 
+const audioSchema = new mongoose.Schema(
+  {
+    songtitle: {
+      type: String,
+      required: true
+    },
+    audio: {
+      type: String,
+      required: true
+    }
+  },
+  { timestamps: true }
+);
+
+const Audio = mongoose.model('Audio', audioSchema);
+
+module.exports = Audio;
